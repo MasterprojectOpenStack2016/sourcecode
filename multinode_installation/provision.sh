bd7a2ad695575f13aae21975a1a6215a3fc1a59b
Your Name <you@example.com>
2016-05-11 17:44:26 +0200

    Added iptable rules for Horizon forwarding

--- multinode_installation/provision.sh
+++ multinode_installation/provision.sh
@@ -14,3 +14,14 @@ sudo git checkout 13.1.0
 sudo scripts/bootstrap-ansible.sh
 sudo scripts/bootstrap-aio.sh
 sudo scripts/run-playbooks.sh
+
+# Setup iptables rules for Horizon forwarding
+sudo iptables -A FORWARD -i eth0 -o eth1 -p tcp --syn --dport 443 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -A FORWARD -i eth0 -o eth1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A FORWARD -i eth1 -o eth0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -P FORWARD DROP
+
+HORIZON_IP=$(sudo lxc-info -n aio1_horizon_container-26dfeeed -iH | tail -n1)
+VM_IP=$(ip -f inet -o addr show eth1|cut -d\  -f 7 | cut -d/ -f 1)
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j DNAT --to-destination $HORIZON_IP
+sudo iptables -t nat -A POSTROUTING -o eth1 -p tcp --dport 443 -d $HORIZON_IP -j SNAT --to-source $VM_IP
